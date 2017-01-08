556175c5ddf5fd8945cda56775592581c6116861
mangadul <muin.abdul@gmail.com>
2017-01-09 00:33:44 +0700

    update login

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -1,24 +1,116 @@
-<div class="login-box">
+        <!-- Top content -->
+        <div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1><strong><?php echo $site_name; ?></strong> Login</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+                        		<div class="form-top-left">
+                        			<h3>Air Minum Ozone</h3>
+                            		<p>Masukan namauser dan kata kunci anda!</p>
+                        		</div>
+                        		<div class="form-top-right">
+                        			<i class="fa fa-key"></i>
+                        		</div>
+                            </div>
+                            <div class="form-bottom">
+			                    <form role="form" action="<?php echo base_url('admin/login/index');?>" method="post" class="login-form">
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="form-username">Username</label>
+			                        	<input type="text" name="username" placeholder="Username..." class="form-username form-control" id="form-username">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="form-password">Password</label>
+			                        	<input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-password">
+			                        </div>
+                                    <input type="hidden" name="remember" value="on">
+			                        <button type="submit" class="btn">Masuk</button>
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 social-login">
+                        	<h3>...or login with:</h3>
+                        	<div class="social-login-buttons">
+	                        	<a class="btn btn-link-1 btn-link-1-facebook" href="#">
+	                        		<i class="fa fa-facebook"></i> Facebook
+	                        	</a>
+	                        	<a class="btn btn-link-1 btn-link-1-twitter" href="#">
+	                        		<i class="fa fa-twitter"></i> Twitter
+	                        	</a>
+	                        	<a class="btn btn-link-1 btn-link-1-google-plus" href="#">
+	                        		<i class="fa fa-google-plus"></i> Google Plus
+	                        	</a>
+                        	</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
 
-	<div class="login-logo"><b><?php echo $site_name; ?></b></div>
 
-	<div class="login-box-body">
-		<p class="login-box-msg">Sign in to start your session</p>
-		<?php echo $form->open(); ?>
-			<?php echo $form->messages(); ?>
-			<?php echo $form->bs3_text('Username', 'username', ENVIRONMENT==='development' ? 'webmaster' : ''); ?>
-			<?php echo $form->bs3_password('Password', 'password', ENVIRONMENT==='development' ? 'webmaster' : ''); ?>
-			<div class="row">
-				<div class="col-xs-8">
-					<div class="checkbox">
-						<label><input type="checkbox" name="remember"> Remember Me</label>
-					</div>
-				</div>
-				<div class="col-xs-4">
-					<?php echo $form->bs3_submit('Sign In', 'btn btn-primary btn-block btn-flat'); ?>
-				</div>
-			</div>
-		<?php echo $form->close(); ?>
-	</div>
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.min.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/form-elements.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/style.css');?>">
 
-</div>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Javascript -->        
+        <script src="<?php echo base_url('assets/login/js/jquery-1.11.1.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/login/js/jquery.backstretch.min.js');?>"></script>
+        <!--
+        <script src="<?php echo base_url('assets/login/js/scripts.js');?>"></script>
+        -->
+
+        <!--[if lt IE 10]>
+            <script src="<?php echo base_url('assets/login/js/placeholder.js');?>"></script>
+        <![endif]-->
+
+<script type="text/javascript">
+jQuery(document).ready(function() {
+    /*
+        Fullscreen background
+    */
+    $.backstretch("<?php echo base_url('assets/login/img/backgrounds/1.jpg');?>");
+    
+    /*
+        Form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+        $(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+        
+        $(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+            if( $(this).val() == "" ) {
+                e.preventDefault();
+                $(this).addClass('input-error');
+            }
+            else {
+                $(this).removeClass('input-error');
+            }
+        });
+        
+    });
+    
+ 
+});
+</script>
